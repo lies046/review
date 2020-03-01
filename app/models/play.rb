4e6e2292b167e06d8c4f861eb413fc3979b52003
lies046 <skytree.skysky@gmail.com>
2020-03-01 15:53:46 +0900

    Change btn

--- app/models/play.rb
+++ app/models/play.rb
@@ -3,6 +3,6 @@ class Play < ApplicationRecord
     belongs_to :category
     has_many :revues
 
-    has_attached_file :play_img, styles: { play_index: "300x300>", play_show: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :play_img, styles: { play_index: "300x300>", play_show: "475x325>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
